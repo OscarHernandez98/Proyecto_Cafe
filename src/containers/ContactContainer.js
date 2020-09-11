cf88effacf951dad676cffcd1fbb84889770c221
OscarHernandez98 <oscar_iktan@hotmail.com>
2020-09-11 15:52:28 -0500

    se realizo contactanos y avance de tienda

--- src/containers/ContactContainer.js
+++ src/containers/ContactContainer.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import "./ContactContainer.css"
 
 const ContactContainer = () => {
     return (
-        <div>
-            <h1>Contactanos</h1>
+        <div className="div">
+            <Form>
+                <section className="intro-section">
+                    <div className="container text-center">
+                        <div className="row">
+                            <div className="col-xl-10 offset-xl-1">
+                                <h2 className="section-title">¡Contactanos!</h2>
+                                <br/>
+                                <h6 className="section-title">Si tienes dudas o comentarios, llena el siguiente formulario</h6>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>Nombre</Form.Label>
+                        <Form.Control type="email" placeholder="Nombre" />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridPassword">
+                        <Form.Label>Correo</Form.Label>
+                        <Form.Control type="password" placeholder="Correo" />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Group controlId="formGridAddress1">
+                    <Form.Label>Dirección</Form.Label>
+                    <Form.Control placeholder="Direccion" />
+                </Form.Group>
+
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Mensaje</Form.Label>
+                    <Form.Control as="textarea" rows="3" />
+                </Form.Group>
+
+                <Button variant="dark" size="lg" block>
+                    Enviar
+            </Button>
+            </Form>
         </div>
     )
 }
--- src/containers/ShopContainer.js
+++ src/containers/ShopContainer.js
@@ -1,11 +1,180 @@
 import React from 'react'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card from 'react-bootstrap/Card'
+import cafe1 from '../assets/cafe1.png'
+import './ShopContainer.css'
 
 const ShopContainer = () => {
-    return (
-        <div>
-            <h1>Tienda en linea</h1>
-        </div>
-    )
+  return (
+    <main className="background">
+      <div className="divShop">
+        <section className="intro-section">
+          <div className="container text-center">
+            <div className="row">
+              <div className="col-xl-10 offset-xl-1 tienda">
+                <h2 className="section-title">Tienda en Línea</h2>
+                <br />
+                <h6 className="section-title tienda">Aquí podras encontrar todos nuestros productos que tenemos para ti</h6>
+                <br />
+                <br />
+                <br />
+              </div>
+            </div>
+          </div>
+        </section>
+        <CardDeck>
+
+          {/* cafe1 */}
+          <Card>
+            <Card.Img variant="top" src={cafe1} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+
+         {/* cafe2 */}
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to additional
+                content.{' '}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+
+        {/* cafe3 */}
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This card has even longer content than the first to
+                show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
+        <br/>
+        <br/>
+
+        {/* cafetera1 */}
+        <CardDeck>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+
+          {/* cafetera2 */}
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to additional
+                content.{' '}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+
+          {/* cafetera3 */}
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This card has even longer content than the first to
+                show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
+        <br/>
+        <br/>
+
+        {/* accesorios1 */}
+        <CardDeck>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+
+          {/* accesorios2 */}
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to additional
+                content.{' '}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+
+          {/* accesorios3 */}
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This card has even longer content than the first to
+                show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
+
+      </div>
+    </main>
+
+  )
 }
 
 export default ShopContainer
